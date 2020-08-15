4c95f1e73761847cb7c2c450fd58ce4c443a1eb3
nickmbrown <61257682+nickmbrown@users.noreply.github.com>
2020-08-15 10:22:17 -0400

    Fixed digit bug and added dark mode (lol)

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+
 let firstValue = 0;
 let operatorValue = '';
 let awaitingNextValue = false;
+
+const calculate = {
+    '/':(firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*':(firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+':(firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-':(firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=':(firstNumber, secondNumber) => secondNumber,
+};
+
+// Calculator Functions
 
 function sendNumberValue(number) {
     if(awaitingNextValue) {
@@ -14,7 +27,6 @@ function sendNumberValue(number) {
         const displayValue = calculatorDisplay.textContent;
         calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
     }
-
 }
 
 function addDecimal() {
@@ -23,16 +35,6 @@ function addDecimal() {
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
     }
 }
-
-const calculate = {
-    '/':(firstNumber, secondNumber) => firstNumber / secondNumber,
-    '*':(firstNumber, secondNumber) => firstNumber * secondNumber,
-    '+':(firstNumber, secondNumber) => firstNumber + secondNumber,
-    '-':(firstNumber, secondNumber) => firstNumber - secondNumber,
-    '=':(firstNumber, secondNumber) => secondNumber,
-
-};
-
 
 function useOperator(operator) {
     const currentValue = Number(calculatorDisplay.textContent);
@@ -44,7 +46,6 @@ function useOperator(operator) {
         firstValue = currentValue;
     } else {
         const calculation = calculate[operatorValue](firstValue, currentValue);
-        console.log('calculation',calculation);
         firstValue = calculation;
         calculatorDisplay.textContent = calculation;
     }
@@ -53,8 +54,25 @@ function useOperator(operator) {
     console.log(firstValue, operatorValue, currentValue);
 } 
 
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
+}
+
+// Dark Mode
+
+function toggleDarkLightMode(isDark) {
+    localStorage.setItem('theme', `${isDark ? 'dark' : 'light'}`);
+    isDark ? toggleIcon.classList.replace('fa-sun', 'fa-moon') : toggleIcon.classList.replace('fa-moon', 'fa-sun');
+    document.documentElement.setAttribute('data-theme', `${isDark ? 'dark' : 'light'}`)
+}
+
+// Event Listeners
+
 inputBtns.forEach((inputBtn) => {
-    if(inputBtn.classList.length === 0) {
+    if(inputBtn.classList.contains('digit')) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if(inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
@@ -63,11 +81,15 @@ inputBtns.forEach((inputBtn) => {
     }
 });
 
-function resetAll() {
-    calculatorDisplay.textContent = '0';
-    firstValue = 0;
-    operatorValue = '';
-    awaitingNextValue = false;
-}
-
 clearBtn.addEventListener('click', resetAll);
+
+toggleSwitch.addEventListener("change", () => toggleDarkLightMode(event.target.checked));
+
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
